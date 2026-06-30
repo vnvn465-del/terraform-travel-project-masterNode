4acac84d816787d11e294e39590aa3056ac2409c
vnvn465-del <vnvn465@gmail.com>
2026-06-30 14:49:58 +0900

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,3 +1,4 @@
+# 마스터 노드
 # EC2 인스턴스 설정: K3s 마스터 노드 역할을 수행하는 서버를 정의합니다.
 resource "aws_instance" "k8s_master" {
   ami           = "ami-03fd85ef2fae79c05" 
@@ -5,7 +6,7 @@ resource "aws_instance" "k8s_master" {
   # K8s 구동을 위한 2vCPU, 8GB RAM 사양 선택
   instance_type = "t3.large"
 
-  # [수정 포인트] 외부 인터넷 연결을 위해 퍼블릭 서브넷(subnet_1, 2a 가용영역)에 배치합니다.
+  #  외부 인터넷 연결을 위해 퍼블릭 서브넷(subnet_1, 2a 가용영역)에 배치합니다.
   subnet_id     = aws_subnet.subnet_1.id 
   
   # 앞서 만든 보안 그룹 연결
@@ -34,10 +35,34 @@ resource "aws_instance" "k8s_master" {
   }
 }
 
-# (만약 워커 노드 코드도 있다면 아래처럼 subnet_2 에 배치해 주세요!)
-# resource "aws_instance" "k8s_worker" {
-#   ...
-#   subnet_id = aws_subnet.subnet_2.id  # 워커 노드는 퍼블릭 서브넷(2b)에 분산 배치
-#   associate_public_ip_address = true
-#   ...
-# }
+
+
+# 워커노드
+# 인스턴스 설정
+resource "aws_instance" "k8s_worker" {
+  ami           = "ami-03fd85ef2fae79c05" 
+  
+  # 마스터 노드와 동일한 2vCPU, 8GB RAM 사양 선택
+  instance_type = "t3.large"
+
+  # [핵심 포인트] 고가용성(HA)을 위해 마스터(subnet_1, 2a)와 다른 가용 영역인 
+  # 퍼블릭 서브넷(subnet_2, ap-northeast-2b)에 분산 배치합니다.
+  subnet_id     = aws_subnet.subnet_2.id 
+  
+  # 보안 그룹 및 키 페어 동일하게 연결
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+  key_name      = "travel-key"
+
+  # EFS 마운트 및 S3 접근을 위한 IAM 역할 부여
+  iam_instance_profile = "My-K8s-Storage-Role"
+  
+  # 외부 인터넷 통신을 위한 퍼블릭 IP 할당
+  associate_public_ip_address = true
+
+
+
+  tags = {
+    Name = "My-Travel-Worker" 
+  }
+}
+
