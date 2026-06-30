6fe60ec8dca7c85ff15616931514c7ebfd324dbb
vnvn465-del <vnvn465@gmail.com>
2026-06-30 14:23:05 +0900

    Update EFS.tf

--- EFS.tf
+++ EFS.tf
@@ -2,7 +2,7 @@
 resource "aws_efs_file_system" "travel_efs" {
   creation_token = "travel-app-efs"
   
-  # 암호화 활성화 상태
+  # 데이터 보안을 위한 암호화 활성화
   encrypted      = true
 
   tags = {
@@ -10,29 +10,27 @@ resource "aws_efs_file_system" "travel_efs" {
   }
 }
 
-# 2. 마운트 타겟 설정 (캡처 확인: 4개 AZ 모두 연결)
-# 각 가용 영역의 서브넷에서 EFS에 접근할 수 있도록 통로를 뚫어줍니다.
+# 2. 마운트 타겟 설정 (프라이빗 서브넷으로 제한)
+# EFS 스토리지를 외부 접근이 차단된 프라이빗 서브넷(2c, 2d)에만 배치하여 보안을 강화합니다.
 
-resource "aws_efs_mount_target" "mount_2a" {
-  file_system_id  = aws_efs_file_system.travel_efs.id
-  subnet_id       = "subnet-0a86f29e25d29c861" # 2a 서브넷 ID
-  security_groups = [aws_security_group.k8s_sg.id]
-}
-
-resource "aws_efs_mount_target" "mount_2b" {
-  file_system_id  = aws_efs_file_system.travel_efs.id
-  subnet_id       = "subnet-0099b6b0280667a14" # 2b 서브넷 ID
-  security_groups = [aws_security_group.k8s_sg.id]
-}
-
+# 가용 영역 2c (Private Subnet 3) 마운트 타겟
 resource "aws_efs_mount_target" "mount_2c" {
   file_system_id  = aws_efs_file_system.travel_efs.id
-  subnet_id       = "subnet-0bc367e1cedbbfe0d" # 2c 서브넷 ID
+  
+  # vpc.tf에서 생성한 subnet_3의 ID를 동적으로 참조
+  subnet_id       = aws_subnet.subnet_3.id 
+  
+  # EFS 접근을 허용하는 보안 그룹 연동 (NFS 포트 허용 필요)
+  security_groups = [aws_security_group.k8s_sg.id] 
+}
+
+# 가용 영역 2d (Private Subnet 4) 마운트 타겟
+resource "aws_efs_mount_target" "mount_2d" {
+  file_system_id  = aws_efs_file_system.travel_efs.id
+  
+  # 하드코딩 대신 vpc.tf에서 생성한 subnet_4의 ID를 동적으로 참조
+  subnet_id       = aws_subnet.subnet_4.id 
+  
   security_groups = [aws_security_group.k8s_sg.id]
 }
 
-resource "aws_efs_mount_target" "mount_2d" {
-  file_system_id  = aws_efs_file_system.travel_efs.id
-  subnet_id       = "subnet-0accbbe235158e573" # 2d 서브넷 ID
-  security_groups = [aws_security_group.k8s_sg.id]
-}
